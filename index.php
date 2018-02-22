31bcef357b77dfcc2f004f139d17353018fc5d5b
César Cará <ccara@localhost.localdomain>
2018-02-22 14:01:55 -0300

    with line breakers now

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <title>Sample Web Page</title>
 
 <body>
-  Welcome to MyExample.com
+  Welcome to MyExample.com <br>
   <?php
     echo date('l, F jS, Y');
+    echo "\n";
     echo $_SERVER['HTTP_USER_AGENT'];
     ?>
 </body>
