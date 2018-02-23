6865802acff0d0671e99acaa90e3130c6c4a70d4
César Cará <ccara@localhost.localdomain>
2018-02-23 14:51:40 -0300

    test function

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
 <body>
   Welcome to MyExample.com <br>
   <?php
+    include('test.php');
     echo date('l, F jS, Y');
     echo "<br>";
     echo $_SERVER['HTTP_USER_AGENT'];
-    include('test.php');
+    test();
     ?>
 </body>
 
