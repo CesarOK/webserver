2abceea1135e1f7dfa5d4ef58c3d5cf012f6b7b9
César Cará <ccara@localhost.localdomain>
2018-02-27 13:17:48 -0300

    nova cara

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
-<html>
-<title>Pagina do Cesar e do Abdala</title>
+<html lang="en">
+
+
+<head>
+	<meta charset="UTF-8">	
+	<title>Pagina do César e do Abdala</title>
+	
+	<style>
+		header {
+			background: #e3e3e3;
+			padding: 2em;
+			text-align: center;
+		}
+	</style>
+
+</head>
+
 
 <body>
-  Welcome to MyExample.com <br>
+  <header>
+  	<h1><?php echo 'Testing website, by César and Badaua' ?></h1>
+  </header>
   <?php
     include('test.php');
     echo date('l, F jS, Y');
