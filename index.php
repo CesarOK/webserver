82c05d879fed35533dda74cd1811c1d6d742e3d3
César Cará <ccara@localhost.localdomain>
2018-03-14 22:41:15 -0300

    login colocado

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php 
 
-$greetings = "Welcome to our site";
-$names = array('Jeff','John','Mary' );
-
-function dd($data)
-{
-	echo '<pre>';
-	die(var_dump($data));
-	echo '<pre>';
-}
-
-$task = [
-	'title' =>	'Finishing Homework',
-	'due'	=>	'today',
-	'assigned_to' => 'Cesar',
-	'completed' => false,
-];
-
-require "index.view.php";
+require 'LoginWebServer.html'
 
  ?>
